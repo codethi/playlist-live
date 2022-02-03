7855af82298bf67b2e0511256fb6ce02c55aefc7
codethi <docencia.thiago@gmail.com>
2022-02-03 20:36:29 -0300

    create

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const connetToDb = require("./database/db");
+const path = require("path");
+const Music = require("./model/Music");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 connetToDb();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
+app.post("/create", async (req, res) => {
+  const music = req.body;
+  await Music.create(music);
+  res.redirect("/")
 });
 
 app.listen(port, () =>
