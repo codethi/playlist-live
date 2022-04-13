5404e13550eec48c006207bcc9c4b60825a4a907
codethi <docencia.thiago@gmail.com>
2022-04-13 15:30:18 -0300

    add delete music

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Music = require("./model/Music");
 const app = express();
 const port = process.env.PORT || 3000;
 let music = null;
+let musicDel = null;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -21,7 +22,7 @@ app.get("/", async (req, res) => {
 
 app.get("/admin", async (req, res) => {
   const playlist = await Music.find();
-  res.render("admin", { playlist, music: null });
+  res.render("admin", { playlist, music: null, musicDel: null });
 });
 
 app.post("/create", async (req, res) => {
@@ -30,16 +31,25 @@ app.post("/create", async (req, res) => {
   res.redirect("/");
 });
 
-app.get("/by/:id", async (req, res) => {
-  const { id } = req.params;
+app.get("/by/:id/:action", async (req, res) => {
+  const { id, action } = req.params;
   music = await Music.findById({ _id: id });
   const playlist = await Music.find();
-  res.render("admin", { playlist, music });
+  if (action == "edit") {
+    res.render("admin", { playlist, music, musicDel: null });
+  } else {
+    res.render("admin", { playlist, music: null, musicDel: music });
+  }
 });
 
 app.post("/update/:id", async (req, res) => {
   const newMusic = req.body;
   await Music.updateOne({ _id: req.params.id }, newMusic);
+  res.redirect("/admin");
+});
+
+app.get("/delete/:id", async (req, res) => {
+  await Music.deleteOne({ _id: req.params.id });
   res.redirect("/admin");
 });
 
