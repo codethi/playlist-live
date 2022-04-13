b489ae90c1816b4ba88502d4313ea91a50887319
codethi <docencia.thiago@gmail.com>
2022-04-13 15:06:00 -0300

    add edit music

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Music = require("./model/Music");
 
 const app = express();
 const port = process.env.PORT || 3000;
+let music = null;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -20,13 +21,26 @@ app.get("/", async (req, res) => {
 
 app.get("/admin", async (req, res) => {
   const playlist = await Music.find();
-  res.render("admin", { playlist });
+  res.render("admin", { playlist, music: null });
 });
 
 app.post("/create", async (req, res) => {
   const music = req.body;
   await Music.create(music);
   res.redirect("/");
+});
+
+app.get("/by/:id", async (req, res) => {
+  const { id } = req.params;
+  music = await Music.findById({ _id: id });
+  const playlist = await Music.find();
+  res.render("admin", { playlist, music });
+});
+
+app.post("/update/:id", async (req, res) => {
+  const newMusic = req.body;
+  await Music.updateOne({ _id: req.params.id }, newMusic);
+  res.redirect("/admin");
 });
 
 app.listen(port, () =>
