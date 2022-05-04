498d02d99bc4628106d08a99752496d65f977391
codethi <docencia.thiago@gmail.com>
2022-05-04 17:38:13 -0300

    volume e progressbar

--- public/script.js
+++ public/script.js
@@ -38,10 +38,13 @@ controls.addEventListener("click", function (event) {
 
     progressbar.max = currentMusic.audio.duration;
     textTotalDuration.innerText = secondsToMinutes(currentMusic.audio.duration);
-    textCurrentDuration.innerText = secondsToMinutes(
-      currentMusic.audio.currentTime
-    );
-    progressbar.valueAsNumber = currentMusic.audio.currentTime;
+
+    currentMusic.audio.ontimeupdate = function () {
+      textCurrentDuration.innerText = secondsToMinutes(
+        currentMusic.audio.currentTime
+      );
+      progressbar.valueAsNumber = currentMusic.audio.currentTime;
+    };
   }
 
   if (event.target.id == "play-control") {
@@ -58,6 +61,29 @@ controls.addEventListener("click", function (event) {
       currentMusic.audio.pause();
       isPlaying = false;
     }
+  }
+
+  if (event.target.id == "vol-icon") {
+    currentMusic.audio.muted = !currentMusic.audio.muted;
+    if (currentMusic.audio.muted) {
+      event.target.classList.replace(
+        "bi-volume-up-fill",
+        "bi-volume-mute-fill"
+      );
+    } else {
+      event.target.classList.replace(
+        "bi-volume-mute-fill",
+        "bi-volume-up-fill"
+      );
+    }
+  }
+
+  if (event.target.id == "volume") {
+    currentMusic.audio.volume = event.target.valueAsNumber / 100;
+  }
+
+  if (event.target.id == "progressbar") {
+    currentMusic.audio.currentTime = event.target.valueAsNumber;
   }
 });
 
